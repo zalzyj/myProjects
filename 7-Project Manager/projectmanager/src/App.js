d0d6158489a4880b0ddc7481d89e28e8f7f1a376
Jacky Jiang <33780519+zalzyj@users.noreply.github.com>
2018-05-29 13:10:15 +1000

    react-to do list

--- 7-Project Manager/projectmanager/src/App.js
+++ 7-Project Manager/projectmanager/src/App.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects  from './Components/Projects';
+import AddProject from './Components/AddProject';
+import uuid from 'uuid';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        projects: []
+    }
+}
+
+componentWillMount(){
+    this.setState({projects:[
+        
+        {
+            id: uuid.v4(),
+            title: 'Business Website',
+            category: 'Web Design'
+        },
+        {
+            id: uuid.v4(),
+            title: 'Social App',
+            category: 'Mobile Development'
+        },
+        {
+            id: uuid.v4(),
+            title: 'Ecommerce Shopping Cart',
+            category: 'Web Development'
+        }
+    
+    ]});
+}
+
+handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+}
+handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index=projects.findIndex(x=>x.id===id);
+    projects.splice(index,1);
+    this.setState({projects: projects});
+}
   render() {
     return (
       <div className="App">
-          My App
+          <AddProject addProject={this.handleAddProject.bind(this)}/>
+          <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
