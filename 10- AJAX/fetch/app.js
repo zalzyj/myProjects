8777d5c28ffd5a3b784ffef21204e0287c5ebbe7
zalzyj <jackyjiang73gmail.com>
2018-06-13 15:06:27 +1000

    update

--- 10- AJAX/fetch/app.js
+++ 10- AJAX/fetch/app.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch');
 var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-fetch(url).then(function(data){
-    console.log(data);
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data.bpi.EUR.rate);
 });
